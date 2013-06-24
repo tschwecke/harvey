780f3f01261b49d06d024d8412dade069ee1dacd
Tim Schwecke <timothy.schwecke@pearson.com>
2013-06-24 14:57:41 -0600

    Defining the public interface for Harvey

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 SuiteBuilder = require('./lib/suiteBuilder.js');
 HarveyStatus = require('./lib/util/status.js');
-harveyStatus = new HarveyStatus();
+
+Harvey = function() {
+
+	var _status = new HarveyStatus();
+	var _suiteBuilder = new SuiteBuilder();
+
+	
+	this.run = function(tests, suite, config, callback) {
+
+		//Start a new call stack when invoking harvey so that all status callbacks
+		//have a consistent scope
+		setTimeout(function() {
+			var suiteInvoker = _suiteBuilder.buildSuite(tests, suite, config, _status);
+			suiteInvoker(callback);
+		}, 1);
+	};
+
+	//Expose all of the methods for tying in to the status events
+	this.onSuiteStarting = _status.onSuiteStarting;
+	this.onSuiteCompleted = _status.onSuiteCompleted;
+	this.onSuiteSetupStarting = _status.onSuiteSetupStarting;
+	this.onSuiteSetupCompleted = _status.onSuiteSetupCompleted;
+	this.onSuiteTeardownStarting = _status.onSuiteTeardownStarting;
+	this.onSuiteTeardownCompleted = _status.onSuiteTeardownCompleted;
+	this.onTestGroupStarting = _status.onTestGroupStarting;
+	this.onTestGroupCompleted = _status.onTestGroupCompleted;
+	this.onTestSetupStarting = _status.onTestSetupStarting;
+	this.onTestSetupCompleted = _status.onTestSetupCompleted;
+	this.onTestTeardownStarting = _status.onTestTeardownStarting;
+	this.onTestTeardownCompleted = _status.onTestTeardownCompleted;
+	this.onTestStarting = _status.onTestStarting;
+	this.onTestCompleted = _status.onTestCompleted;
+	this.onRequestStarting = _status.onRequestStarting;
+	this.onRequestCompleted = _status.onRequestCompleted;
+	this.onRequestFailed = _status.onRequestFailed;
+};
