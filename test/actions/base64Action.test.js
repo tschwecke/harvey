045f3e84faf7c8fee0aa2cec9af592625cd19af5
Jeans, Rayland <Rayland.Jeans@pearson.com>
2013-09-17 12:09:55 -0600

    Add base64 action

---
+++ test/actions/base64Action.test.js
@@ -0,0 +1,63 @@
+var assert = require('assert'),
+	_ = require('underscore'),
+	Base64Action = require('../../lib/actions/base64Action.js'),
+	util = require('../../lib/util/util.js');
+
+describe('base64Action', function() {
+
+	describe('constructor()', function() {
+
+		it('should return an object with a perform method', function(done) {
+
+			//Act
+			var action = new Base64Action();
+
+			//Assert
+			assert(action);
+			assert(_.isFunction(action.perform));
+
+			done();
+		});
+	});
+
+	describe('perform()', function() {
+
+		it('should base64 encode a string', function(done) {
+
+			var encodingInfo = {
+				"value": "Test"
+			};
+
+			var action = new Base64Action(encodingInfo, util.parseValue);
+
+			//Act
+			var replacedString = action.perform();
+
+			//Assert
+			assert.equal(replacedString, 'VGVzdA==');
+			done();
+		});
+
+		it('should base64 encode a string using variables', function(done) {
+
+			var encodingInfo = {
+				"value": "${str}"
+			};
+
+			var variables = {
+				"str": "Test"
+			}
+
+			var action = new Base64Action(encodingInfo, util.parseValue);
+
+			//Act
+			var replacedString = action.perform(null, null, variables);
+
+			//Assert
+			assert.equal(replacedString, 'VGVzdA==');
+			done();
+		});
+
+	});
+
+});
